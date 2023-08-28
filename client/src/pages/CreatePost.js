df0b5cb295f50cd31ed422cb15ab9a2b1f9f7f95
agmatyagi1717 <agamtyagi1717@gmail.com>
2023-08-28 16:47:17 +0530

    createpost link

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -19,7 +19,7 @@ const CreatePost = () => {
 
     e.preventDefault();
 
-    const response = await fetch('http://localhost:4000/post', {
+    const response = await fetch('https://mern-blog-agam.onrender.com/post', {
         method: 'POST',
         body: data,
         credentials: 'include',
