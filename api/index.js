dc8383c2601d67bf48a4c071819d3af09a2dff55
agmatyagi1717 <agamtyagi1717@gmail.com>
2023-08-29 00:04:31 +0530

    jwt

--- api/index.js
+++ api/index.js
@@ -74,7 +74,7 @@ app.get("/profile", (req, res) => {
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie("token");
+  res.clearCookie("jwt");
 });
 
 app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
