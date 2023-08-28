2e6a28418a8fd8a04bd1d7ca71a20535385e2378
agmatyagi1717 <agamtyagi1717@gmail.com>
2023-08-28 14:46:49 +0530

    access origin

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ const fs = require("fs");
 const salt = bcrypt.genSaltSync(10);
 const secret = "bnfawjbfwafw2r2";
 
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: "https://mern-blog-opal.vercel.app" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
