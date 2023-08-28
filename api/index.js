6328c1e5f3497b0c52ac62d513b48476dbaa67c1
agmatyagi1717 <agamtyagi1717@gmail.com>
2023-08-28 18:15:56 +0530

    d

--- api/index.js
+++ api/index.js
@@ -64,21 +64,20 @@ app.post("/login", async (req, res) => {
 app.get("/profile", (req, res) => {
   const { token } = req.cookies;
   
-  if(token){
-    jwt.verify(token, secret, {}, (err, info) => {
-      if (err) throw err;
-      
-      res.json(info);
-    });
-  }
+  
+  jwt.verify(token, secret, {}, (err, info) => {
+    if (err) throw err;
+    
+    res.json(info);
+  });
   res.json(req.cookies);
 });
 
 
 
-// app.post("/logout", (req, res) => {
-//   res.cookie('token', '').json("ok");
-// });
+app.post("/logout", (req, res) => {
+  res.cookie('token', '').json("ok");
+});
 
 app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   const { originalname, path } = req.file;
